c3437240f5ffefd7e9fcf835cad134af31626790
oleksandr.revebtsov <oleksandr.revebtsov@gmail.com>
2022-06-07 03:45:16 +0300

    Add unstaged file

--- ContactManager/Common/Constants.swift
+++ ContactManager/Common/Constants.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+enum Constants {
+    static let apiUrl = "https://mocki.io/v1/1f8336ef-28dc-4516-a3a6-bd8443599986"
+}
--- ContactManager/Controller/ViewController.swift
+++ ContactManager/Controller/ViewController.swift
@@ -6,23 +6,56 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ViewController: UIViewController {
-    var names = [String]()
-    var sectionTitle = [String]()
-    var namesDict = [String: [String]]()
+    
+    @IBOutlet weak var contactsTableView: UITableView!
+    private var names = [String]()
+    private var sectionTitle = [String]()
+    private var namesDict = [String: [String]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        names = ["Alexandra",  "Alison",  "Amanda",  "Amelia",  "Amy",  "Andrea",  "Angela",  "Anna",  "Anne",  "Audrey",  "Ava",  "Bella",  "Bernadette",  "Carol",  "Caroline",  "Carolyn",  "Chloe",  "Claire",  "Deirdre",  "Diana",  "Diane",  "Donna",  "Dorothy",  "Elizabeth",  "Ella",  "Emily",  "Emma",  "Faith",  "Felicity",  "Fiona",  "Gabrielle",  "Grace",  "Hannah",  "Heather",  "Irene",  "Jan",  "Jane",  "Jasmine",  "Jennifer",  "Jessica",  "Joan",  "Joanne",  "Julia",  "Karen",  "Katherine",  "Kimberly",  "Kylie",  "Lauren",  "Leah",  "Lillian",  "Lily",  "Lisa",  "Madeleine",  "Maria",  "Mary",  "Megan",  "Melanie",  "Michelle",  "Molly",  "Natalie",  "Nicola",  "Olivia",  "Penelope",  "Pippa",  "Rachel",  "Rebecca",  "Rose",  "Ruth",  "Sally",  "Samantha",  "Sarah",  "Sonia",  "Sophie",  "Stephanie",  "Sue",  "Theresa",  "Tracey",  "Una",  "Vanessa",  "Victoria",  "Virginia",  "Wanda",  "Wendy",  "Yvonne",  "Zoe",  "Adam",  "Adrian",  "Alan",  "Alexander",  "Andrew",  "Anthony",  "Austin",  "Benjamin",  "Blake",  "Boris",  "Brandon",  "Brian",  "Cameron",  "Carl",  "Charles",  "Christian",  "Christopher",  "Colin",  "Connor",  "Dan",  "David",  "Dominic",  "Dylan",  "Edward",  "Eric",  "Evan",  "Frank",  "Gavin",  "Gordon",  "Harry",  "Ian",  "Isaac",  "Jack",  "Jacob",  "Jake",  "James",  "Jason",  "Joe",  "John",  "Jonathan",  "Joseph",  "Joshua",  "Julian",  "Justin",  "Keith",  "Kevin",  "Leonard",  "Liam",  "Lucas",  "Luke",  "Matt",  "Max",  "Michael",  "Nathan",  "Neil",  "Nicholas",  "Oliver",  "Owen",  "Paul",  "Peter",  "Phil",  "Piers",  "Richard",  "Robert"
-        ]
+        loadData()
+        prepareDataForTableView()
+        setSectionTitle()
+    }
+}
+extension ViewController {
+    func loadData() {
+        let url = URL(string: Constants.apiUrl)!
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                DispatchQueue.main.async {
+                    do {
+                        let listOf = try JSONDecoder().decode(ListofContacts.self, from: data)
+                        DataBaseManager.shared.save(contacts: listOf.contacts)
+                    }
+                    catch let error {print (error)}
+                }
+                
+            } else if let error = error {
+                print("HTTP Request Failed: \(error)")
+            }
+        }
+        
+        task.resume()
+    }
+    
+    func prepareDataForTableView() {
+        let dataFromDb = DataBaseManager.shared.getContacts()
+        for result in dataFromDb {
+            names.append(result.firstName ?? "")
+        }
+    }
+    
+    func setSectionTitle() {
         sectionTitle = Array(Set(names.compactMap({String($0.prefix(1))})))
         sectionTitle.sort()
         sectionTitle.forEach({namesDict[$0] = [String]()})
         names.forEach({namesDict[String($0.prefix(1))]?.append($0)})
-        
     }
-}
-extension ViewController: UITableViewDelegate {
     
 }
 
@@ -30,7 +63,7 @@ extension ViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         sectionTitle.count
     }
-  
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         namesDict[sectionTitle[section]]?.count ?? 0
     }
@@ -40,10 +73,9 @@ extension ViewController: UITableViewDataSource {
         cell.textLabel?.text = namesDict[sectionTitle[indexPath.section]]?[indexPath.row]
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         sectionTitle[section]
     }
     
 }
-
